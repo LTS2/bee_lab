ebe91a14ca5987a532c9801f3b58c909194c23d2
ejum01 <ejum0713@gmail.com>
2023-11-23 12:54:34 +0900

    Fix : 메인화면 듀오구하기 수정

--- src/main/java/com/bee/beelab/searchSummoner/restAPI/SearchSummonerRestController.java
+++ src/main/java/com/bee/beelab/searchSummoner/restAPI/SearchSummonerRestController.java
@@ -1,7 +1,6 @@
 package com.bee.beelab.searchSummoner.restAPI;
 
 import com.bee.beelab.searchSummoner.model.entity.SearchSummonerDTO;
-import com.bee.beelab.searchSummoner.model.entity.SearchSummonerRankedDTO;
 import com.bee.beelab.searchSummoner.service.SearchSummonerServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
 
 
 @Slf4j
