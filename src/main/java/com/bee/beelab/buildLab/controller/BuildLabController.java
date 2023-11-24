5bc1a6cedd180dec3c9f8e6baccc907ef4a882d3
이민주 <minjooo0116@gmail.com>
2023-11-24 17:54:01 +0900

    Fix: buildLab으로 컨트롤러 이름 수정

--- src/main/java/com/bee/beelab/buildLab/controller/BuildLabController.java
+++ src/main/java/com/bee/beelab/buildLab/controller/BuildLabController.java
@@ -1,8 +1,7 @@
-package com.bee.beelab.build.controller;
+package com.bee.beelab.buildLab.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * 빌드연구소 컨트롤러
